f60f7f3cb8b5f1beb74f574185285ce519a74c34
normyee <lunoqro@gmail.com>
2025-08-04 22:15:19 -0300

    test: add test container db connection to mongodb

--- apps/budget-service/test/budget-spec.ts
+++ apps/budget-service/test/budget-spec.ts
@@ -1,5 +1,31 @@
+import mongoose from 'mongoose';
+import {
+  MongoDBContainer,
+  StartedMongoDBContainer,
+} from '@testcontainers/mongodb';
+
+export class CreateBudgetUseCase {}
+
+// PASSOS
+// 1 - criar a classe testada. no caso 'CreateBudgetUseCase'
+// 2 - criar mock/spy das suas dependências para simular o retorno controlado
+// 3 - a implementação do repository será pelo mock/spy
+
+let mongodbContainer: StartedMongoDBContainer;
+
+beforeAll(async () => {
+  mongodbContainer = await new MongoDBContainer('mongo:6.0.1').start();
+  const uri = mongodbContainer.getConnectionString();
+  await mongoose.connect(uri);
+});
+
+afterAll(async () => {
+  await mongoose.disconnect();
+  await mongodbContainer.stop();
+});
+
 describe('Budget use cases', () => {
-  it('should create a budget', () => {
+  it('should create a budget', async () => {
     const sut = new CreateBudgetUseCase();
 
     expect();
